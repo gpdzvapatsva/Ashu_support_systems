3696614bc5101413a79dce7be28deb8bb793b82a
godwin <gpdzvapatsva@gmail.com>
2026-05-13 14:10:57 +0100

    added allowed hosts

--- Support_task/settings.py
+++ Support_task/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-)1urf(lk$wo$*4s^$r43ewt9*)bw_ds^k#lboyk$(kevpt50_5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
